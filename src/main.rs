85a21bb7fcd2faaf619c1dc7493d19398488825d
benjamin <benjamin@gnbit.com>
2020-10-16 16:14:39 -0500

    Add conection reference

--- src/main.rs
+++ src/main.rs
@@ -12,10 +12,13 @@ async fn main() -> Result<(), anyhow::Error> {
 
     let database_url = env::var("DATABASE_URL").expect("DATABASE_URL is not set in .env file");
 
-    let db_pool = PgPool::new(&database_url)
-    .await
+    let db_pool = PgPool::builder().max_size(5).build(database_url).await
     .map_err(anyhow::Error::from)
     .with_context(|| "Failed to connect to Postgres.")?;
+
+    /*let db_pool = PgPool::new(&database_url)
+    .await*/
+   
 
     let listener = TcpListener::bind("127.0.0.1:5161")?;
     run(listener, db_pool)?.await?;
